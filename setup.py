fb3d707e33e9b6de68365993f828b2dc590db6bd
hbenedek <hben.0204@gmail.com>
2023-04-25 13:59:00 +0200

    fix path

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setuptools.setup(
     ],
     entry_points={
         "console_scripts": [
-            "visiumlint = visiumlint.main:lint",
+            "visiumlint = src.visiumlint.main:lint",
         ],
     },
 )
