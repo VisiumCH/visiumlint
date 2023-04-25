e1e45cbed3aaa19200e8f29d2050b8fee1d79f9f
hbenedek <hben.0204@gmail.com>
2023-04-25 14:56:48 +0200

    add bumpversion

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="visiumlint",
-    version="0.0.2",
+    version="0.0.1",
     author="Visium SA",
     description="Visium linting tool",
     long_description=long_description,
